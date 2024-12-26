5a9a621a2aacf20623ca68a94b8831abacaed708
GeraltdDima <dimalegendarypro@gmail.com>
2024-12-26 12:49:04 +0200

    LessonProgram

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public class Program
+{
+	public static IEnumerable<int> GetSortedNumbers(
+			IEnumerable<int> numbers) {
+		return numbers.OrderBy(number => number);
+	}
+
+	// Example
+	// int[] numbers = { 43, 32, 734, 45 };
+	//
+	//GetSortedNumbers(numbers)
+	//>>>>> output
+	//
+	//{32, 43, 45, 734}
+	
+	//Question
+	//Make program that takes numbers from user
+	//and writes sum numbers and sorted numbers.Program must be by cycle
+}
